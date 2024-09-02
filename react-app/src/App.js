68a2c1488e97ffad83f1a9dd6db5af7b6952ae60
nsirtak <nsirtak@gmail.com>
2024-09-01 23:16:57 -0500

    initial commit

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import CostItem from "./components/CostItem";
+
+function App() {
+  return (
+    <div> 
+      <h1>Start studying React</h1>
+      <CostItem></CostItem>
+      </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/CostItem.js
@@ -0,0 +1,9 @@
+function CostItem() {
+    return (<div>
+        <div>Date</div>
+        <h2>Title</h2>
+        <div>Sum</div>
+    </div>)
+    
+}
+export default CostItem
